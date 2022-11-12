6de4d162697326f6e3f6d54f032d086190a3676f
Felipe Aguiar <felipe.pimentel.aguiar@gmail.com>
2022-11-12 14:54:26 -0600

    make whole form controllable

--- src/components/PanelSettings/index.tsx
+++ src/components/PanelSettings/index.tsx
@@ -13,31 +13,33 @@ import {
   Stack,
   VStack
 } from '@chakra-ui/react'
+import React, { FormEvent, useState } from 'react'
 import ColorPicker from '../base/ColorPicker'
 import Radio from '../base/Radio'
-import React, { useState } from 'react'
-// import { useForm } from 'react-hook-form'
-
-// type Inputs = {
-//   borderWidth: number
-//   borderRadius: number
-//   borderColor: string
-//   shadowType: string
-//   shadowColor: string
-//   rotationType: string
-// }
 
 type ShadowStyle = 'None' | 'Light' | 'Moderate' | 'Strong'
-type Rotation = 0 | 90 | -90 | 180
+type Rotation = '0' | '90' | '-90' | '180'
 
 function PanelSettings (): JSX.Element {
-  // const { handleSubmit, register } = useForm<Inputs>()
   const [borderWidth, setBorderWidth] = useState(1)
   const [borderRadius, setBorderRadius] = useState(0)
   const [borderColour, setBorderColour] = useState('000000')
   const [shadowStyle, setShadowStyle] = useState<ShadowStyle>('None')
   const [shadowColour, setShadowColour] = useState('000000')
-  const [rotation, setRotation] = useState<Rotation>(0)
+  const [rotation, setRotation] = useState<Rotation>('0')
+
+  const handleSubmit = (_event: FormEvent): boolean => {
+    console.log({
+      borderWidth,
+      borderRadius,
+      borderColour,
+      shadowStyle,
+      shadowColour,
+      rotation
+    })
+
+    return false
+  }
 
   return (
     <VStack spacing="24px" align="start">
@@ -46,7 +48,12 @@ function PanelSettings (): JSX.Element {
         <HStack spacing={2}>
           <FormControl>
             <FormLabel>Width</FormLabel>
-            <NumberInput defaultValue={0} max={30} clampValueOnBlur={false}>
+            <NumberInput
+              value={borderWidth}
+              onChange={(_, value) => setBorderWidth(value)}
+              max={30}
+              min={0}
+            >
               <NumberInputField />
               <NumberInputStepper>
                 <NumberIncrementStepper />
@@ -56,7 +63,12 @@ function PanelSettings (): JSX.Element {
           </FormControl>
           <FormControl>
             <FormLabel>Radius</FormLabel>
-            <NumberInput defaultValue={0} max={30} clampValueOnBlur={false}>
+            <NumberInput
+              value={borderRadius}
+              onChange={(_, value) => setBorderRadius(value)}
+              max={16}
+              min={0}
+            >
               <NumberInputField />
               <NumberInputStepper>
                 <NumberIncrementStepper />
@@ -77,11 +89,13 @@ function PanelSettings (): JSX.Element {
       <Heading size="md" mb={1}>Shadow</Heading>
         <Radio
           direction="column"
+          selected={shadowStyle}
+          onChange={setShadowStyle}
           options={[
-            { label: 'None', value: 'none' },
-            { label: 'Light', value: 'light' },
-            { label: 'Moderate', value: 'moderate' },
-            { label: 'Strong', value: 'strong' }
+            { label: 'None', value: 'None' },
+            { label: 'Light', value: 'Light' },
+            { label: 'Moderate', value: 'Moderate' },
+            { label: 'Strong', value: 'Strong' }
           ]}
         />
         <ColorPicker
@@ -93,17 +107,24 @@ function PanelSettings (): JSX.Element {
       <Heading size="md" mb={1}>Rotation</Heading>
         <Radio
           direction="column"
+          selected={rotation}
+          onChange={setRotation}
           options={[
-            { label: 'None', value: 0 },
-            { label: '90˚ (clock-wise)', value: 90 },
-            { label: '90˚ (counterclock-wise)', value: -90 },
-            { label: '180˚', value: 180 }
+            { label: 'None', value: '0' },
+            { label: '90˚ (clock-wise)', value: '90' },
+            { label: '90˚ (counterclock-wise)', value: '-90' },
+            { label: '180˚', value: '180' }
           ]}
         />
       </Box>
       <Box>
         <Stack direction="row" justifyContent="space-around">
-          <Button width="100px">Ok</Button>
+          <Button
+            width="100px"
+            onClick={handleSubmit}
+          >
+            Save
+          </Button>
           <Button width="100px">Cancel</Button>
         </Stack>
       </Box>
